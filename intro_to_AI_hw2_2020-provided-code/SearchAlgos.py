cfabf2c29179ed7cd2ad902a35151f4edf72d530
shiran1992s@gmail.com <47076788+shiran1992s@users.noreply.github.com>
2020-12-12 18:03:57 +0200

    MinimaxPlayer First Step

--- intro_to_AI_hw2_2020-provided-code/SearchAlgos.py
+++ intro_to_AI_hw2_2020-provided-code/SearchAlgos.py
@@ -2,7 +2,8 @@
 """
 from utils import ALPHA_VALUE_INIT, BETA_VALUE_INIT
 #TODO: you can import more modules, if needed
-
+import MinimaxPlayer
+from MinimaxPlayer import Player as player
 
 class SearchAlgos:
     def __init__(self, utility, succ, perform_move, goal):
@@ -17,6 +18,7 @@ class SearchAlgos:
         self.utility = utility
         self.succ = succ
         self.perform_move = perform_move
+        self.goal = goal
 
     def search(self, state, depth, maximizing_player):
         pass
@@ -32,7 +34,36 @@ class MiniMax(SearchAlgos):
         :return: A tuple: (The min max algorithm value, The direction in case of max node or None in min mode)
         """
         #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        if player.check_time():
+            player.time_ended = True
+        if player.time_ended:
+            return -1
+        if self.goal(state, maximizing_player):
+            return self.utility(state)
+        if depth == 0:
+            return player.heuristic(state, maximizing_player)
+
+        self_moves_tuple, rival_moves_tuple = MinimaxPlayer.available_moves_handler(state, state.location,
+                                                                                    state.rival_location)
+
+        moves = self_moves_tuple[1] if maximizing_player else rival_moves_tuple[1]
+        max_result = float("-inf")
+        min_result = float("inf")
+        selected_move = None
+        for move in moves:
+            state.set_move(move, maximizing_player)
+            move_value = self.search(state, not maximizing_player, depth - 1)
+            state.reset_move(move, maximizing_player)
+            if maximizing_player and max_result < move_value:
+                max_result = move_value
+                selected_move = move
+            elif not maximizing_player and min_result > move_value:
+                min_result = move_value
+
+        if maximizing_player:
+            return max_result, selected_move
+        else:
+            return min_result, None
 
 
 class AlphaBeta(SearchAlgos):
--- intro_to_AI_hw2_2020-provided-code/players/MinimaxPlayer.py
+++ intro_to_AI_hw2_2020-provided-code/players/MinimaxPlayer.py
@@ -3,12 +3,28 @@ MiniMax Player
 """
 from players.AbstractPlayer import AbstractPlayer
 #TODO: you can import more modules, if needed
-
+import time
+import utils
 
 class Player(AbstractPlayer):
     def __init__(self, game_time, penalty_score):
         AbstractPlayer.__init__(self, game_time, penalty_score) # keep the inheritance of the parent's (AbstractPlayer) __init__()
         #TODO: initialize more fields, if needed, and the Minimax algorithm from SearchAlgos.py
+        self.penalty_score = penalty_score
+        self.location = None
+        self.game_board = None
+        self.rival_location = None
+        self.rival_points = 0
+        self.points = 0
+        self.time_ended = False
+        self.start_time = 0
+        self.fruit_locations = dict()
+        self.best_fruit_value = 0
+        self.best_fruit_location = None
+        self.moves_available = []
+        self.moves_available_count = 0
+        self.rival_moves_available = []
+        self.rival_moves_available_count = 0
 
 
     def set_game_params(self, board):
@@ -20,7 +36,26 @@ class Player(AbstractPlayer):
         No output is expected.
         """
         #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+
+        # TODO: Check if need to update the fruit locations in here or only players.
+        self.game_board = board
+        # number_of_players = 2
+        for row_index, row_value in board:
+            for cell_index, cell_value in row_value:
+                if cell_value == 1:
+                    self.location = (row_index, cell_index)
+                    # number_of_players -= 1
+                if cell_value == 2:
+                    self.rival_location = (row_index, cell_index)
+                    # number_of_players -= 1
+                # if number_of_players is 0:
+                #     break
+                if cell_value > 2:
+                    location = (row_index, cell_index)
+                    self.fruit_locations.update({location: cell_value})
+                    if cell_value > self.best_fruit_value:
+                        self.best_fruit_value = cell_value
+                        self.best_fruit_location = (row_index, cell_index)
 
     def make_move(self, time_limit, players_score):
         """Make move with this Player.
@@ -30,7 +65,6 @@ class Player(AbstractPlayer):
             - direction: tuple, specifing the Player's movement, chosen from self.directions
         """
         #TODO: erase the following line and implement this function.
-        raise NotImplementedError
 
 
     def set_rival_move(self, pos):
@@ -40,7 +74,15 @@ class Player(AbstractPlayer):
         No output is expected
         """
         #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        self.game_board[self.rival_location[0]][self.rival_location[1]] = -1
+        cell_value = self.game_board[pos[0]][pos[1]]
+        if cell_value > 2:
+            self.rival_points += cell_value
+            self.fruit_locations.pop(pos)
+            if pos == self.best_fruit_location:
+                self.find_best_fruit()
+        self.game_board[pos[0]][pos[1]] = 2
+        self.rival_location = pos
 
 
     def update_fruits(self, fruits_on_board_dict):
@@ -52,12 +94,56 @@ class Player(AbstractPlayer):
         No output is expected.
         """
         #TODO: erase the following line and implement this function. In case you choose not to use it, use 'pass' instead of the following line.
-        raise NotImplementedError
+        self.fruit_locations = fruits_on_board_dict
 
 
     ########## helper functions in class ##########
     #TODO: add here helper functions in class, if needed
+    def find_best_fruit(self):
+        best_location = max(self.fruit_locations, key=self.fruit_locations.get)
+        best_value = self.fruit_locations[best_location]
 
+        if best_value >= self.best_fruit_value:
+            self.best_fruit_value = best_value
+            self.best_fruit_location = best_location
+
+    def check_time(self):
+        return time.time() - self.start_time > self.time_limit - 0.01
 
     ########## helper functions for MiniMax algorithm ##########
     #TODO: add here the utility, succ, and perform_move functions used in MiniMax algorithm
+
+    def available_moves_handler(self, location, rival_location):
+        board = self.board
+        available_moves, rival_available_moves = [], []
+        available_moves_count, rival_available_moves_count = 0, 0
+
+        directions = utils.get_directions()
+        for d in directions:
+            self_i = location[0] + d[0]
+            self_j = location[1] + d[1]
+            rival_i = rival_location[0] + d[0]
+            rival_j = rival_location[1] + d[1]
+            if 0 <= self_i < len(board) and 0 <= self_j < len(board[0]) and board[self_i][self_j] == 0:
+                available_moves.append((self_i, self_j))
+                available_moves_count += 1
+            if 0 <= rival_i < len(board) and 0 <= rival_j < len(board[0]) and board[rival_i][rival_j] == 0:
+                rival_available_moves.append((rival_i, rival_j))
+                rival_available_moves_count += 1
+
+        self_moves_tuple = (available_moves, available_moves_count)
+        rival_moves_tuple = (rival_available_moves, rival_available_moves_count)
+
+        return self_moves_tuple, rival_moves_tuple
+
+
+#TODO: very basic need to consider fruits positions and values
+    def utility(self, current_state):
+        self_moves_tuple, rival_moves_tuple = self.available_moves_handler(self,
+                                                   current_state.location, current_state.rival_location)
+        if self_moves_tuple[1] is 0 and self.rival_moves_tuple[1] is 0:
+            return 0
+        if self.self_moves_tuple[1] >= 1:
+            return 1
+        return -1
+
